ff32f5898a6d174e6304715531b05cc5487d8d04
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2018-06-10 22:25:09 -0500

    :tada: Initial commit

---
+++ fmlolc/commands.py
@@ -0,0 +1,89 @@
+# coding: utf-8
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+# public items
+__all__ = ['initialize',
+           'start_fm',
+           'stop_fm',
+           'finalize']
+
+# standard library
+from logging import getLogger
+
+# dependent packages
+import fmlolc
+
+# module logger
+logger = getLogger(__name__)
+
+
+# functions
+def initialize(fmp_file, lo_freq, multiply=8):
+    """Initialize 1st LO SG and XFFTS for FM mode.
+
+    Args:
+        fmp_file (str or path): Path of FM pattern file.
+        lo_freq (float): LO frequency at FM frequency = 0 in units of Hz.
+        multiply (int): Multiplication factor between SG to LO frequency.
+
+    """
+    listfreq = fmlolc.listfreq(fmp_file, lo_freq, multiply)
+
+    with fmlolc.SCPI(**fmlolc.INFO_XFFTS) as xffts:
+        xffts('XFFTS:CMDUSEDSECTIONS 1 1 1 1')
+        xffts('XFFTS:CMDSYNCTIME 200000')
+        xffts('XFFTS:CMDBLANKTIME 5000')
+        xffts('XFFTS:CONFIG')
+
+    with fmlolc.SCPI(**fmlolc.INFO_SG) as sg:
+        sg('*RST')
+        sg('*CLS')
+        sg('FREQ:MODE CW')
+        sg('OUTP ON')
+        sg('INIT:CONT OFF')
+        sg('LIST:TYPE LIST')
+        sg('LIST:DWEL 2.0E-01')
+        sg('LIST:FREQ {0}'.format(listfreq))
+        sg('LIST:TRIG:SOUR EXT')
+        sg('FREQ:MODE LIST')
+        sg('TRIG:SLOP POS')
+
+
+def start_fm():
+    """Start FM mode."""
+    with fmlolc.SCPI(**fmlolc.INFO_XFFTS) as xffts:
+        # spacify commands if necessary
+        pass
+
+    with fmlolc.SCPI(**fmlolc.INFO_SG) as sg:
+        sg('*RST')
+        sg('*CLS')
+        sg('INIT:CONT ON')
+
+
+def stop_fm():
+    """Stop FM mode."""
+    with fmlolc.SCPI(**fmlolc.INFO_XFFTS) as xffts:
+        # spacify commands if necessary
+        pass
+
+    with fmlolc.SCPI(**fmlolc.INFO_SG) as sg:
+        # spacify commands if necessary
+        pass
+
+    finalize()
+
+
+def finalize():
+    """Revert 1st LO SG and XFFTS to non FM mode."""
+    with fmlolc.SCPI(**fmlolc.INFO_XFFTS) as xffts:
+        xffts('XFFTS:CMDUSEDSECTIONS 1 1 1 1')
+        xffts('XFFTS:CMDSYNCTIME 200000')
+        xffts('XFFTS:CMDBLANKTIME 1000')
+        xffts('XFFTS:CONFIG')
+
+    with fmlolc.SCPI(**fmlolc.INFO_SG) as sg:
+        sg('FREQ:MODE CW')
---
+++ fmlolc/info.py
@@ -0,0 +1,25 @@
+# coding: utf-8
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+# public items
+__all__ = ['INFO_SG',
+           'INFO_XFFTS']
+
+# standard library
+from logging import getLogger
+
+# module logger
+logger = getLogger(__name__)
+
+
+# module constants
+INFO_SG    = {'host': '',
+              'port': '',
+              'protocol': 'TCP'}
+
+INFO_XFFTS = {'host': '',
+              'port': '',
+              'protocol': 'UDP'}
