e13646ee09a2f43804afda56180104ca39ccfc1f
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2018-06-10 22:26:28 -0500

    :+1: Update import

--- fmlolc/__init__.py
+++ fmlolc/__init__.py
@@ -1,8 +1,13 @@
 # coding: utf-8
 
+from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
-from __future__ import unicode_literals
 
-from .cmds import *
+from .info import *
 from .utils import *
+from .commands import *
+
+del info
+del utils
+del commands
